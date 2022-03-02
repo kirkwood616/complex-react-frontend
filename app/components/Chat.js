df0441db6a857230a1d58331d57fa4280b549256
kirkwood616 <kirkwood616@gmail.com>
2022-03-02 18:10:49 -0500

    Finishing Chat

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -2,12 +2,14 @@ import React, { useEffect, useContext, useRef } from "react";
 import StateContext from "../StateContext";
 import DispatchContext from "../DispatchContext";
 import { useImmer } from "use-immer";
+import { Link } from "react-router-dom";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:8080");
 
 function Chat() {
   const chatField = useRef(null);
+  const chatLog = useRef(null);
   const appState = useContext(StateContext);
   const appDispatch = useContext(DispatchContext);
   const [state, setState] = useImmer({
@@ -18,6 +20,7 @@ function Chat() {
   useEffect(() => {
     if (appState.isChatOpen) {
       chatField.current.focus();
+      appDispatch({ type: "clearUnreadChatCount" });
     }
   }, [appState.isChatOpen]);
 
@@ -28,6 +31,13 @@ function Chat() {
       });
     });
   }, []);
+
+  useEffect(() => {
+    chatLog.current.scrollTop = chatLog.current.scrollHeight;
+    if (state.chatMessages.length && !appState.isChatOpen) {
+      appDispatch({ type: "incrementUnreadChatCount" });
+    }
+  }, [state.chatMessages]);
 
   function handleFieldChange(e) {
     const value = e.target.value;
@@ -60,11 +70,11 @@ function Chat() {
           <i className="fas fa-times-circle"></i>
         </span>
       </div>
-      <div id="chat" className="chat-log">
+      <div ref={chatLog} id="chat" className="chat-log">
         {state.chatMessages.map((message, index) => {
           if (message.username == appState.user.username) {
             return (
-              <div className="chat-self" key={index}>
+              <div key={index} className="chat-self">
                 <div className="chat-message">
                   <div className="chat-message-inner">{message.message}</div>
                 </div>
@@ -74,15 +84,15 @@ function Chat() {
           }
 
           return (
-            <div className="chat-other">
-              <a href="#">
+            <div key={index} className="chat-other">
+              <Link to={`/profile/${message.username}`}>
                 <img className="avatar-tiny" src={message.avatar} />
-              </a>
+              </Link>
               <div className="chat-message">
                 <div className="chat-message-inner">
-                  <a href="#">
+                  <Link to={`/profile/${message.username}`}>
                     <strong>{message.username}: </strong>
-                  </a>
+                  </Link>
                   {message.message}
                 </div>
               </div>
--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -20,13 +20,7 @@ function HeaderLoggedIn(props) {
   }
   return (
     <div className="flex-row my-3 my-md-0">
-      <a
-        data-for="search"
-        data-tip="Search"
-        onClick={handleSearchIcon}
-        href="#"
-        className="text-white mr-2 header-search-icon"
-      >
+      <a data-for="search" data-tip="Search" onClick={handleSearchIcon} href="#" className="text-white mr-2 header-search-icon">
         <i className="fas fa-search"></i>
       </a>
       <ReactTooltip place="bottom" id="search" className="custom-tooltip" />{" "}
@@ -34,10 +28,10 @@ function HeaderLoggedIn(props) {
         onClick={() => appDispatch({ type: "toggleChat" })}
         data-for="chat"
         data-tip="Chat"
-        className="mr-2 header-chat-icon text-white"
+        className={"mr-2 header-chat-icon " + (appState.unreadChatCount ? "text-danger" : "text-white")}
       >
         <i className="fas fa-comment"></i>
-        <span className="chat-count-badge text-white"> </span>
+        {appState.unreadChatCount ? <span className="chat-count-badge text-white">{appState.unreadChatCount < 10 ? appState.unreadChatCount : "9+"}</span> : ""}
       </span>
       <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />{" "}
       <Link data-for="profile" data-tip="My Profile" to={`/profile/${appState.user.username}`} className="mr-2">
